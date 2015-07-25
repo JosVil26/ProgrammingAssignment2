58bd2b09bc60aa66db4c4f6270c65b263bd4deec
José <josvil20@hotmail.com>
2015-07-25 10:08:11 -0500

    a little improve to the comments in cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,10 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 ## My functions permit to calculate the inverse of a matrix by using a cache
-## solution
+## method
 
 ## Write a short comment describing this function
-## This function create a special vector wich contain operations to get and set
+## This function creates a special vector wich contain operations to get and set
 ## the matrix and the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -23,8 +23,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-## This function return the inverse of 'x' by search in the cache and if isn't 
-## found return by the solve function
+## This function returns the inverse of 'x' by search in the cache and if isn't 
+## found return the inverse by the solve function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
